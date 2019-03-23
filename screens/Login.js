1311c0ac195e0e6b3790b1fb957c982242130b1a
ademcan <ademcan@ademcan.net>
2019-03-23 10:46:47 +0100

    Login workflow complete

--- screens/Login.js
+++ screens/Login.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
 
 export default class Login extends Component {
 
+  state={
+    email: '',
+    pwd: ''
+  }
+
+  onEmailChange = (email) => {
+    this.setState({email : email})
+  }
+
+  onPwdChange = (pwd) => {
+    this.setState({pwd : pwd})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Component</Text>
+        <Text style={{textAlign:'center', fontSize:24, color:"#253041"}}>Welcome to Relai</Text>
+        <View style={{alignItems:'center', paddingTop:50}}>
+          <View>
+            <TextInput onChangeText={text => this.onPwdChange(text)} secureTextEntry={true} style={{width: 300, backgroundColor: 'white', height: 50, borderColor: 'black', borderWidth: 1,  justifyContent:'center'}} placeholder='Email'/>
+          </View>
+          <View style={{paddingTop:30}}>
+            <TextInput onChangeText={text => this.onPwdChange(text)} secureTextEntry={true} style={{ width: 300, backgroundColor: 'white', height: 50, borderColor: 'black', borderWidth: 1,   justifyContent:'center'}} placeholder='Password'/>
+          </View>
+
+          <TouchableHighlight onPress={() => {this.props.navigation.navigate("Main")}} style={{width:300, height:60, backgroundColor: "#253041", justifyContent:'center', marginTop:30}}>
+            <Text style={{color:"white",textAlign:'center', fontSize: 18}}>Login</Text>
+          </TouchableHighlight>
+        </View>
+
+        
+
       </View>
     );
   }
@@ -20,5 +50,7 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop:200
+
   },
 });
