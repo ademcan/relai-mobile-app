2ed1663124fa1cc39df186b539bb6aa953dfa465
Stefaan Ponnet <stefaan@ponnet.com>
2019-03-23 20:03:06 +0100

    FAQ

--- screens/Faq1.js
+++ screens/Faq1.js
@@ -12,10 +12,11 @@ export default class Faq1 extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{alignItems:'center', paddingTop: 100}}>
-            <Text style={{fontSize:20}}>You invested</Text>
-            <Text style={{fontSize:50}}>100 CHF</Text>
-            <Text style={{fontSize:20}}>per month</Text>
+        <View style={{alignItems:'center', paddingTop: 50, paddingLeft: 20,paddingright: 20}}>
+            {/* <Text style={{fontSize:20}}>You invested</Text> */}
+            <Text style={{fontSize:40}}>What is Dollar Cost Averaging?</Text>
+            {/* <Text style={{fontSize:20}}>by Saskia</Text> */}
+            <Image resizeMode={"contain"} style={{width:300}} source={require('../resources/images/faq1.png')}/>
         </View>
       </View>
     );
--- screens/Faq2.js
+++ screens/Faq2.js
@@ -12,10 +12,10 @@ export default class Faq2 extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{alignItems:'center', paddingTop: 100}}>
-            <Text style={{fontSize:20}}>You invested</Text>
-            <Text style={{fontSize:50}}>100 CHF</Text>
-            <Text style={{fontSize:20}}>per month</Text>
+        <View style={{alignItems:'center', paddingTop: 50, paddingLeft: 20,paddingright: 20}}>
+            {/* <Text style={{fontSize:20}}>You invested</Text> */}
+            <Text style={{fontSize:40}}>Fundamentals of Bitcoin</Text>
+            <Text style={{fontSize:20,paddingTop: 10}}>Every discussion about Bitcoin sooner or later leads to a wrangle over what gives Bitcoin value. Skeptics claim it has no value whatsoever, while believers see it as digital gold with nearly infinite potential. So who is right and what actually makes single Bitcoin valuable? There are several attributes to consider.</Text>
         </View>
       </View>
     );
